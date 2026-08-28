4d1f34b081e53d38947c96d6df9d41e3b96ed4c5
Phetso <phetso.nkosi@umuzi.org>
2026-08-28 17:15:50 +0200

    added test for the navigation

--- fitness-tracker/src/components/Navigation/Navbar.jsx
+++ fitness-tracker/src/components/Navigation/Navbar.jsx
@@ -2,16 +2,13 @@ import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 function Navbar(){
     return (
-        <header className={styles.header}>
-            <h1>OnTrack</h1>
-            <ul className={styles.navigation}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/exercises">Exercises</Link></li>
-                <li><Link to="/workout-planner">Workout-Planner</Link></li>
-                <li><Link to="/progress">Progress</Link></li>
-                <li><Link to="/history">History</Link></li>
-            </ul>
-        </header>
+        <ul className={styles.navigation}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/exercises">Exercises</Link></li>
+            <li><Link to="/workout-planner">Workout-Planner</Link></li>
+            <li><Link to="/progress">Progress</Link></li>
+            <li><Link to="/history">History</Link></li>
+        </ul>
     )
 }
 
---
+++ fitness-tracker/src/components/Navigation/Navbar.test.jsx
@@ -0,0 +1,32 @@
+import { test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Navbar from "./Navbar";
+
+test("should render navigation links.", () => {
+    render(
+        <MemoryRouter>
+            <Navbar />
+        </MemoryRouter>
+    );
+
+    expect(
+        screen.getByRole("link", { name: "Home" })
+    ).toBeInTheDocument();
+
+    expect(
+        screen.getByRole("link", { name: "Exercises" })
+    ).toBeInTheDocument();
+
+    expect(
+        screen.getByRole("link", { name: "Workout-Planner" })
+    ).toBeInTheDocument();
+
+    expect(
+        screen.getByRole("link", {name: "Progress"})
+    ).toBeInTheDocument();
+
+    expect(
+        screen.getByRole("link", {name: "History"})
+    ).toBeInTheDocument();
+});

e656cdb27eb28f46e896fa994a478c5ca46c7924
Phetso <phetso.nkosi@umuzi.org>
2026-08-30 20:32:47 +0200

    added styling for the button component

--- fitness-tracker/src/components/Navigation/Navbar.jsx
+++ fitness-tracker/src/components/Navigation/Navbar.jsx
@@ -1,23 +1,73 @@
 import { Link, useLocation } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
-function activeRoute(route){
+function Navbar({ isOpen }) {
     const location = useLocation();
-    return location.pathname === route ? `${styles.active}` : "" ;
-}
-
-function Navbar({isOpen}){
- 
 
     return (
         <ul className={`${styles.navigation} ${isOpen ? styles.open : ""}`}>
-            <li><Link to="/" className={ activeRoute("/")}>Home</Link></li>
-            <li><Link to="/exercises" className={ activeRoute("/exercises") }>Exercises</Link></li>
-            <li><Link to="/workout-planner" className={ activeRoute("/workout-planner") }>Workout-Planner</Link></li>
-            <li><Link to="/progress" className={ activeRoute("/progress") }>Progress</Link></li>
-            <li><Link to="/history" className={ activeRoute("/history") }>History</Link></li>
+            <li>
+                <Link
+                    to="/"
+                    className={location.pathname === "/" ? styles.active : ""}
+                >
+                    Home
+                </Link>
+            </li>
+
+            <li>
+                <Link
+                    to="/exercises"
+                    className={
+                        location.pathname === "/exercises"
+                            ? styles.active
+                            : ""
+                    }
+                >
+                    Exercises
+                </Link>
+            </li>
+
+            <li>
+                <Link
+                    to="/workout-planner"
+                    className={
+                        location.pathname === "/workout-planner"
+                            ? styles.active
+                            : ""
+                    }
+                >
+                    Workout-Planner
+                </Link>
+            </li>
+
+            <li>
+                <Link
+                    to="/progress"
+                    className={
+                        location.pathname === "/progress"
+                            ? styles.active
+                            : ""
+                    }
+                >
+                    Progress
+                </Link>
+            </li>
+
+            <li>
+                <Link
+                    to="/history"
+                    className={
+                        location.pathname === "/history"
+                            ? styles.active
+                            : ""
+                    }
+                >
+                    History
+                </Link>
+            </li>
         </ul>
-    )
+    );
 }
 
 export default Navbar;

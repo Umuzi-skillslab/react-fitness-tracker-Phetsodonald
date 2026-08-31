acae188300120bc7393c1d12ba0579dc7a7a093f
Phetso <phetso.nkosi@umuzi.org>
2026-08-31 15:58:52 +0200

    styled the modal component and its container

--- fitness-tracker/src/components/UI/Modal.jsx
+++ fitness-tracker/src/components/UI/Modal.jsx
@@ -4,7 +4,7 @@ import styles from "./UI.module.css";
 
 const Modal = ({children, title="Details", onClose}) => {
     return(
-        <div className={styles.modalBackdrop} role="dialog" aria-modal="true">
+        <div className={styles.modalContainer} role="dialog" aria-modal="true">
             <div className={styles.modal}>
                 <h2>{title}</h2>
                 {children}

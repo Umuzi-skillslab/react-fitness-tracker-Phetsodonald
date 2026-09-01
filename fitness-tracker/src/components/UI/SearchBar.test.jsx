7962266d7d5df8823fad2d19cc6195916593087a
Phetso <phetso.nkosi@umuzi.org>
2026-09-01 11:33:09 +0200

    added test for the search bar component

---
+++ fitness-tracker/src/components/UI/SearchBar.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import SearchBar from "./SearchBar";
+import { describe, expect, test } from "vitest";
+import userEvent from "@testing-library/user-event";
+
+describe("SearchBar", () => {
+    test("should update and submit search", async () => {
+        const func = jest.fn();
+        const user = userEvent.setup();
+
+        render(<SearchBar onSearch={func} onClear={jest.fn()}/>);
+        await user.type(screen.getAllByPlaceholderText("Search exercises.."), "push");
+        await user.click(screen.getByRole("button", {name: /search/i}));
+
+        expect(func).toHaveBeenCalled("push")
+    })
+
+    test('should clear calls callback',async()=>{
+        const clear=jest.fn();
+        const user=userEvent.setup();
+        
+        render(<SearchBar onSearch={jest.fn()} onClear={clear} searchTerm="test"/>);
+        
+        await user.click(screen.getByRole('button',{name:'Clear'}));
+        expect(clear).toHaveBeenCalled()})
+});

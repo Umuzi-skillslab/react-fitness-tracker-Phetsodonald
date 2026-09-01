e6ecaa5ce1e46f0f76f7f226d331d605529f3a39
Phetso <phetso.nkosi@umuzi.org>
2026-09-01 14:22:41 +0200

    added the get streak helper function

--- fitness-tracker/src/utils/helpers.js
+++ fitness-tracker/src/utils/helpers.js
@@ -10,4 +10,15 @@ export const calculateTotalCalories = exercises => {
 
 export const calculateTotalWeight = workout => {
     return (workout ?.sets || 0) * (workout ?.reps || 0) * (workout ?.weight || 0);
-}
+};
+
+export const getStreak = history => {
+    if (!history.length) return 0;
+    let streak = 1;
+    for (let i=1;i<dates.length;i++) {
+        const diff = (new Date(dates[i-1]) - new Date(dates[i])) / 86400000;
+        if (diff === 1) streak++; else break;
+    }
+    return streak;    
+};
+

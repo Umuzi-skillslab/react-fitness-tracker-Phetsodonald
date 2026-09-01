2ca9846cf014f7e32739dbd3adacb1c85e58c285
Phetso <phetso.nkosi@umuzi.org>
2026-09-01 14:13:29 +0200

    added the calulate total calories helper function

--- fitness-tracker/src/utils/helpers.js
+++ fitness-tracker/src/utils/helpers.js
@@ -2,3 +2,9 @@ export const formatDuration = minutes => {
     return `${minutes} min`
 }
 
+export const calculateTotalCalories = exercises => {
+    return exercises.reduce((previousValue, currentValue) => {
+        previousValue + Number(currentValue.caloriesBurn || 0), 0
+    });
+};
+

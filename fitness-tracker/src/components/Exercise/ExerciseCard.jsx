b3d31d333154514ef5e988b089e2c710b7424270
Phetso <phetso.nkosi@umuzi.org>
2026-09-01 14:06:30 +0200

    added a helper to format duration

---
+++ fitness-tracker/src/components/Exercise/ExerciseCard.jsx
@@ -0,0 +1,5 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import Badge from "../UI/Badge";
+import Card from "../UI.Card";
+import styles from "./Exercise.module.css";
---
+++ fitness-tracker/src/utils/helpers.js
@@ -0,0 +1,4 @@
+export const formatDuration = minutes => {
+    return `${minutes} min`
+}
+

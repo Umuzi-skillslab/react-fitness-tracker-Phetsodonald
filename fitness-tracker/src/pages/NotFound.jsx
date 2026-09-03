48fd04fb9e239fca54e6144551b45325fe4e78c3
Phetso <phetso.nkosi@umuzi.org>
2026-09-03 11:37:09 +0200

    updated the not found page

--- fitness-tracker/src/pages/NotFound.jsx
+++ fitness-tracker/src/pages/NotFound.jsx
@@ -1,7 +1,13 @@
-function NotFound(){
-    return (
-        <h1>Page not Found</h1>
-    )
-}
+import {useNavigate} from 'react-router-dom';
+import Header from '../components/common/Header';
+import Button from '../components/UI/Button';
+
+const NotFound=()=>{
+    const navigate=useNavigate();
+    return <><Header title="404 - Page Not Found" subtitle="The page you are looking for does not exist."/>
+    <Button onClick={()=>navigate('/')}>Go Home</Button>
+    </>
+    
+};
 
 export default NotFound;
